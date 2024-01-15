15029d66f3bc39f0a546b98f1b72e33d43671035
AllynH <AllynH+github@gmail.com>
2024-01-15 19:04:54 +0000

    adding empty dir to store API scraping modules.

---
+++ api_modules/meetup.py
@@ -0,0 +1,5 @@
+'''
+    Put code for Meetup API acraping here. 
+    Update this docstring with details of how to use the module.
+'''
+
---
+++ api_modules/sessionize.py
@@ -0,0 +1,5 @@
+'''
+    Put code for Sessionize API acraping here. 
+    Update this docstring with details of how to use the module.
+'''
+
